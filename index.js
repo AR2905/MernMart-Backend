48d8bf3b22c78ccf8cedb26c7340fb1b4df6ba6b
AR2905 <125748114+AR2905@users.noreply.github.com>
2024-08-10 20:19:04 +0530

    test cat

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ server.use(express.json()); // to parse req.body
 
 server.use('/products', isAuth(), productsRouter.router);
 // we can also use JWT token for client-only auth
-server.use('/categories', isAuth(), categoriesRouter.router);
+// server.use('/categories', isAuth(), categoriesRouter.router);
 server.use('/brands', isAuth(), brandsRouter.router);
 server.use('/users', isAuth(), usersRouter.router);
 server.use('/auth', authRouter.router);
